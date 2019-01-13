7cc2cfabe704bf5bf9fcf162e20e6233b48cf6d7
ndy-01 <stuart@lizardland.co.uk>
2019-01-13 12:18:17 +0000

    Latest revision, 10x10 grid working but won't draw other dimensions

--- etchasketch.js
+++ etchasketch.js
@@ -1,21 +1,44 @@
 const screenRes = 480;
+const maxRows = 10;
+const maxCols = 10;
 
 // div gridBox is the container for the grid
 
 drawBox();
+setListeners();
 
-function drawBox(){
-    let gridBox = document.getElementById('temp');
-
-    drawBlankRows(5);
+function setListeners(){
+    document.addEventListener('mouseover', function(e){
+        if (e.target.tagName == "SPAN"){
+            let thisBox = document.getElementById(e.target.id);
+            thisBox.classList.toggle('highlight');
+        }
+    })
 }
 
-function createBox(size){
+
+function drawBox(){
+    drawBlankRows(maxRows);
+    addBoxesToRows(maxCols,maxRows);
+}
+
+function addBoxesToRows(x,y){
+    for (let thisRow = 0; thisRow < y; thisRow++){
+        let selectedRow = document.getElementById(thisRow.toString());
+        for (let thisBox = 0; thisBox < x; thisBox++){
+            selectedRow.appendChild(createBox(10,thisBox,thisRow));
+        }
+    }
+}
+
+function createBox(size,x,y){
     let sizeInPx = Math.floor(screenRes/size);
-    let newBox = document.createElement('div');
+    let newBox = document.createElement('span');
+    let boxID = x.toString() + y.toString();
     console.log(sizeInPx);
 
     newBox.className = 'simpleBox';
+    newBox.id = boxID;
     return newBox;
 }
 
@@ -30,6 +53,6 @@ function createRow(x){
     let newRow = document.createElement('div');
 
     newRow.className = "row";
-    newRow.innerHTML = "<p>This is a row</p>";
+    newRow.id = x.toString();
     return newRow;
 }
